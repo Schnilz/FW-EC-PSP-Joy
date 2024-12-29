2bdbfadaac13f6a45da261a5b24698e674d4b0c1
Nils Schulte <git@nilsschulte.de>
2024-12-29 10:35:15 +0100

    initial Joystick version

---
+++ firmware/gamepad_config.h
@@ -0,0 +1,11 @@
+#ifndef _GAMEPAD_CONFIG_H
+#define _GAMEPAD_CONFIG_H
+
+#define GAMEPAD_LOGICAL_MIN (10)
+#define GAMEPAD_LOGICAL_MAX (0xFF - 10)
+
+#define STR_MANUFACTURER u"schnils"
+#define STR_PRODUCT u"Framwork Gamepad"
+
+
+#endif
